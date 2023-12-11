b6060013fbbb6dd5300ad6399432a6a0764a2bb5
NikoAs59 <saenennicolas23@gmail.com>
2023-12-11 21:50:17 +0100

    ok

---
+++ index.js
@@ -0,0 +1,7 @@
+const mouses = document.querySelectorAll(".mousemove");
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
+});
